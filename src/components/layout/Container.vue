aa5e64d0c3fa5920c9afbd2b56730339386f4af0
Gabe Dunn <gabe@gabedunn.dev>
2020-09-17 10:38:44 -0700

    add container and section components

---
+++ src/components/layout/Container.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="container">
+		<slot />
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Container'
+	}
+</script>
+
+<style scoped>
+	.container {
+		@apply max-w-screen-2xl px-4;
+
+		/* Small (sm) */
+		@media (min-width: 640px) {
+			@apply px-6;
+		}
+
+		/* Large (lg) */
+		@media (min-width: 1024px) {
+			@apply px-8;
+		}
+	}
+</style>
---
+++ src/components/layout/Sec.vue
@@ -0,0 +1,36 @@
+<template>
+	<main v-if="main" :id="id" class="section">
+		<slot />
+	</main>
+	<section v-else :id="id" class="section">
+		<slot />
+	</section>
+</template>
+
+<script>
+	export default {
+		name: 'Sec',
+		props: {
+			id: {
+				type: String,
+				required: true
+			},
+			main: {
+				type: Boolean,
+				required: false,
+				default: false
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	.section {
+		@apply my-6;
+
+		/* Medium (md) */
+		@media (min-width: 768px) {
+			@apply my-12;
+		}
+	}
+</style>
