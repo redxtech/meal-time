2793e162989434f0bcc8624a539f99978f3e5bd8
Gabe Dunn <gabe@gabedunn.dev>
2020-09-20 23:00:51 -0700

    add modal to meals

--- src/components/sections/Meals.vue
+++ src/components/sections/Meals.vue
@@ -2,8 +2,14 @@
 	<sec id="meals" main>
 		<section-title>Meals</section-title>
 		<div class="meals">
-			<meal v-for="meal in meals" :key="meal.title" :meal="meal" />
+			<meal
+				v-for="meal in meals"
+				:key="meal.title"
+				:meal="meal"
+				@click="openModal(meal)"
+			/>
 		</div>
+		<meal-modal :open="isModalOpen" @close="closeModal" :meal="modalState" />
 	</sec>
 </template>
 
@@ -11,14 +17,43 @@
 	import Sec from '../layout/Sec.vue'
 	import SectionTitle from '../titles/SectionTitle.vue'
 	import Meal from '../meal.vue'
+	import MealModal from '../elements/MealModal.vue'
 
 	import { meals } from '../../assets/content/meals.json'
 
 	export default {
 		name: 'Meals',
-		components: { Sec, Meal, SectionTitle },
+		components: { MealModal, Sec, Meal, SectionTitle },
 		data() {
-			return { meals: [...meals] }
+			return {
+				meals: [...meals],
+				isModalOpen: false,
+				modalState: {
+					title: undefined,
+					description: undefined,
+					directions: undefined,
+					ingredients: undefined,
+					type: undefined,
+					mealtimes: undefined
+				}
+			}
+		},
+		methods: {
+			openModal(meal) {
+				this.isModalOpen = true
+				this.modalState = meal
+			},
+			closeModal() {
+				this.isModalOpen = false
+				this.modalState = {
+					title: undefined,
+					description: undefined,
+					directions: undefined,
+					ingredients: undefined,
+					type: undefined,
+					mealtimes: undefined
+				}
+			}
 		}
 	}
 </script>
